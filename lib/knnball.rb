e4be162b6e387dd18fc8b8507cf02145d0bd9668
yuzixun <yzxun@qq.com>
2016-05-21 19:02:39 +0800

    修复维度错误的问题你

--- lib/knnball.rb
+++ lib/knnball.rb
@@ -30,7 +30,7 @@ module KnnBall
     # 数组的中间数
     median_idx = median_index(data)
     # 将指定维度的数据排序
-    data.sort_by! { |index| index[:point][actual_dimension-1] }
+    data.sort_by! { |index| index[:point][actual_dimension] }
     # 取出中间的值，作为根节点
     value = data[median_idx]
     ball = Ball.new(value)
